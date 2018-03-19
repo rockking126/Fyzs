bf8d341f5db87c33a89cc7ada8c72feccdbba60a
alien <rockking126@gmail.com>
2018-03-19 10:20:05 +0800

    init

--- app/src/main/java/com/yictec/rock/fyzs/MainActivity.java
+++ app/src/main/java/com/yictec/rock/fyzs/MainActivity.java
@@ -4,12 +4,27 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.yictec.rock.fyzs.APi;
+import com.yictec.rock.fyzs.Network;
+import com.yictec.rock.fyzs.News;
+import com.yictec.rock.fyzs.NewsModel;
+import com.yictec.rock.fyzs.R;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private TextView mTextMessage;
+    private Button button_a;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,9 +51,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gettk("client_credentials"
+                ,"amoMVOwvDS513xUAIGhpZp3I"
+                ,"dSLiGRSpSC1IW2v5oVxGvRCoTlXIuPOT");
+
         mTextMessage = (TextView) findViewById(R.id.message);
+        button_a = (Button) findViewById(R.id.button);
+
+
+
+        button_a.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mTextMessage.setText(NewsModel.get_s()+"88");
+            }
+        });
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    public void gettk(String grant_type, String client_id, String client_secret) {
+        APi api = Network.getInstance().getApi();
+        Call<News> news = api.baidu(grant_type, client_id, client_secret);
+        news.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+
+                News body = response.body();
+                Log.i("onResponse:   =", body.access_token);
+                mTextMessage.setText(body.access_token.toString()+"8888");
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+                Log.i("onResponse:   =", t.getMessage());
+            }
+        });
+    }
 }
+
+
